57f1612aaa861d33f6095e778f87c6ce8ae15811
MUGDHA CHOUDHARY <143961893+Mugdha3011@users.noreply.github.com>
2024-07-25 11:05:08 +0530

    Add files via upload

---
+++ 01_basics/num_maths.js
@@ -0,0 +1,44 @@
+const score = 400
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+
+
+const num = 323.34543435
+console.log(num.toFixed(2)); //upto two decimal places 
+
+console.log(num.toPrecision(3));
+
+const hundreds = 100000000
+console.log(hundreds.toLocaleString());//will give commas acc to number system
+
+
+//######## Maths ############
+
+//positive value
+console.log(Math.abs(-5));
+
+ //round off value
+console.log(Math.round(4.5));
+
+// upper value .. means output would be 7
+console.log(Math.ceil(6.3));  
+
+//lower value .. means o/p would be 3
+console.log(Math.floor(3.7));
+
+//max and min value 
+console.log(Math.max(1,34,2,35,67));
+console.log(Math.min(6,4,3,676,32))
+
+//one more important math fn is math.random.. study the mdn doc about it 
+
+
+
+
+
+
+
+
+
